bdca31c1441e4476546d3e54e69a63b61665db7f
enricoricciardella <enrico.ricciardella@gmail.com>
2024-11-12 16:29:47 +0100

    snack 2

---
+++ snack 2/js/script.js
@@ -0,0 +1,24 @@
+const squadre = [
+    { nome:"Giuventus", punti: 0, falli: 0 },
+    { nome:"Milanina", punti: 0, falli: 0 },
+    { nome:"Intero", punti: 0, falli: 0 },
+    { nome:"Alguer", punti: 0, falli: 0 },
+    { nome:"Chiagliari", punti: 0, falli: 0 },
+    { nome:"Amburger", punti: 0, falli: 0 }
+];
+//Genero numeri random con ciclo for
+console.log(`Squadre con numeri random`, squadre);
+for(let i = 0; i < squadre.length; i++ ){
+    const currSquadre = squadre[i];
+    currSquadre.punti = Math.floor(Math.random() * 100 + 1);
+    currSquadre.falli = Math.floor(Math.random() * 29 + 1);
+}
+//Creo nuova array dove salvare e stampare solo Nomi e Falli
+const nomiFalli = [];
+for(let i = 0; i < squadre.length; i ++){
+    const currNomiFalli = squadre[i];
+    nomiFalli.push({ nome: currNomiFalli.nome, falli: currNomiFalli.falli });
+    // console.log(currNomiFalli.nome , currNomiFalli.falli);
+    
+}
+console.log("Array nomi e falli", nomiFalli);
